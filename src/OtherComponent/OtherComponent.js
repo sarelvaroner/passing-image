f9476f40e62fef78d2dc2d5b4c0ee47d93005ee7
Sarel <sarhel.s@gmail.com>
2020-06-07 07:04:19 +0300

    first commit

---
+++ src/OtherComponent/OtherComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OtherComponent({logo}) {
+  return (
+    <div className="OtherComponent">
+        <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default OtherComponent;
